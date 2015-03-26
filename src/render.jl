33e504ea950e1fae5889c80264f92d7965e8923d
Michael Hatherly <michaelhatherly@gmail.com>
2015-03-26 22:54:46 +0200

    Add build script, remove generated html site content.

--- src/render.jl
+++ src/render.jl
@@ -18,7 +18,7 @@ Currently supported formats: `HTML`, and `markdown`.
 
 Beginning with Lexicon 0.1 you can save documentation as pre-formatted
 markdown files which can then be post-processed using 3rd-party programs
-such as the static site generator [MkDocs](https://www.mkdocs.org).
+such as the static site generator [MkDocs](http://www.mkdocs.org).
 
 For details on how to build documentation using MkDocs please consult their
 detailed guides and the Docile and Lexicon packages. A more customized build
