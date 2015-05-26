cd1fb7fd178b79dcdab11ec0a3cd70912f14708a
Michael Hatherly <michaelhatherly@gmail.com>
2015-05-26 20:24:54 +0200

    Blank slate for Lexicon 0.2.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,4 @@ OS_NAME == :Windows && Pkg.add("FactCheck") # Hack for appveyor.
 
 module LexiconTests
 
-using Docile
-@document
-
-using Docile.Interface, Lexicon, FactCheck
-
-import Lexicon: Query
-
-require(joinpath(dirname(@__FILE__), "testcases.jl"))
-
-import TestCases: f, g, A, T, @m, K, S
-
-include("facts/query-parsing.jl")
-include("facts/querying-results.jl")
-include("facts/rendering.jl")
-include("facts/filtering.jl")
-
 end
