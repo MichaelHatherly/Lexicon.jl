59dd8577f3d754ada09b6b032a9b485b348adc1b
Michael Hatherly <michaelhatherly@gmail.com>
2015-08-16 15:27:26 +0200

    Add 'files' function. Fixup autogen header.

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -3,10 +3,16 @@
 """
 module Utilities
 
-export tryget, submodules
+export tryget, submodules, files
 
 tryget(mod, field, default) = isdefined(mod, field) ? getfield(mod, field) : default
 
+"""
+    submodules(mod)
+
+> The set of submodules of a module ``mod``.
+
+"""
 function submodules(mod :: Module)
     out = Set([mod])
     for name in names(mod, true)
@@ -20,5 +26,22 @@ end
 
 validmodule(mod :: Module, object :: Module) = object ≠ mod && object ≠ Main
 validmodule(:: Module, other)                = false
+
+"""
+    files(cond, root)
+
+> Collect all files from a directory matching a condition ``cond``.
+
+By default the file search is recursive. This can be disabled using the keyword
+argument ``recursive = false``.
+"""
+function files(cond, root, out = Set(); recursive = true)
+    for f in readdir(root)
+        f = joinpath(root, f)
+        isdir(f)  && recursive && files(cond, f, out)
+        isfile(f) && cond(f)   && push!(out, f)
+    end
+    out
+end
 
 end
